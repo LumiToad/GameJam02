f3c6073712a0f911c8f49208cf407ed6829125d8
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2023-11-30 16:47:04 +0100

    updated boxen rigidbody collision

--- Assets/Scripts/box.cs
+++ Assets/Scripts/box.cs
@@ -4,11 +4,16 @@ public class box : MonoBehaviour
 {
     public float freezeDuration;
     float timer = 0;
+    
 
     public void Freeze()
     {
         GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<Collider>().enabled = false;
+        if (GetComponent<BoxCollider>() != null)
+        {
+            GetComponent<Collider>().enabled = false;
+
+        }
         transform.parent = FindObjectOfType<Wagen>().gameObject.transform;
         timer = freezeDuration;
     }
@@ -25,7 +30,19 @@ public class box : MonoBehaviour
     public void Unfreeze()
     {
         GetComponent<Rigidbody>().isKinematic = false;
-        GetComponent<Collider>().enabled = true;
+        if (GetComponent<BoxCollider>() != null)
+        {
+            GetComponent<Collider>().enabled = true;
+        }
         transform.SetParent(null);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Ground" && GetComponent<BoxCollider>() != null)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            Destroy(GetComponent<BoxCollider>());
+        }
     }
 }
